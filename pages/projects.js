c58b43468c00434c662f14efc266e81ce46cfbc3
Victor <ukokjnr@gmail.com>
2023-08-06 04:05:10 +0100

    Projec change

--- pages/projects.js
+++ pages/projects.js
@@ -13,29 +13,47 @@ export default function RecipeReviewCard() {
         <div className='all-header' style={{ textAlign: 'center', color: 'white' }}>Projects</div>
 
         <Row gutter={16} style={{ textAlign: 'left', color: 'white' }}>
-        <Col span={24}><h3 style={{ textAlign: 'center', color: 'white' }}>Contract Projects</h3></Col>
+        <Col span={24}><h3 style={{ textAlign: 'center', color: 'white' }}>Frontend Projects</h3></Col>
         
-        <Col className='space' sm={24} md={12}>
-        <a href='https://realitypacesettersfoundation.ng' target='_blank' rel="noreferrer"><Image src='/project-pictures/reality-pacesetters.png' width='280' height='150' className='picture-style' title='Click to view' alt='Reality Pacesetters Foundation' /></a>
-            <h3 style={{ color: 'white' }}>Reality Pace Setters Foundation (NGO)</h3>
-            <h4 style={{ color: 'white' }}>(Contract Job)</h4>
-            <a href='https://realitypacesettersfoundation.ng' target='_blank' rel="noreferrer">View Website</a>
+        
+        <Col className='space' sm={24} md={6}>
+        <a href='#' target='_blank' rel="noreferrer"><Image src='/project-pictures/trovi.png' width='280' height='150' className='picture-style' title='Click to view' alt='Reality Pacesetters Foundation' /></a>
+            <h3 style={{ color: 'white' }}>Trovi</h3>
+        </Col>
+        
+
+        <Col className='space' sm={24} md={6}>
+        <a href='https://remeaccess.org/' target='_blank' rel="noreferrer"><Image src='/project-pictures/remeaccess.png' width='280' height='150' className='picture-style' title='Click to view' alt='Reality Pacesetters Foundation' /></a>
+            <h3 style={{ color: 'white' }}>Reme Access</h3>
         </Col>
 
           
         </Row>
 
         <Row gutter={16} style={{ textAlign: 'left', color: 'white' }}>
-        <Col span={24}><h3 style={{ textAlign: 'center', color: 'white' }}>Personal Projects</h3></Col>
-        
-        <Col className='space' sm={24} md={12}>
+        <Col span={24}><h3 style={{ textAlign: 'center', color: 'white' }}>Full Stack Projects</h3></Col>
+
+        <Col className='space' sm={24} md={6}>
         <a href='https://realitypacesettersfoundation.ng' target='_blank' rel="noreferrer"><Image src='/project-pictures/VNote.png' width='280' height='150' className='picture-style' title='Click to view' alt='realty pace foundation website' /></a>
             <h3 style={{ color: 'white' }}>VNote</h3>
             <h4 style={{ color: 'white' }}>Short note taking website.</h4>
-            <a href='https://vnoteapp.netlify.app/' target='_blank' rel="noreferrer">View Website</a><br />
-            <a href='https://www.postman.com/Victor-peter/workspace/vpublic-documentation/collection/9557443-92ebd656-f2d3-4d43-a195-d02c62c283d1?ctx=documentation' target='_blank' rel="noreferrer">View API Documentation</a><br />
             <a href='https://github.com/victuk/note-taking-project' target='_blank' rel="noreferrer">View Frontend on GitHub</a><br />
             <a href='https://github.com/victuk/Note-taking-api' target='_blank' rel="noreferrer">View Backend on GitHub</a>
+        </Col>
+
+        <Col className='space' sm={24} md={6}>
+        <a href='https://bartcash.netlify.app' target='_blank' rel="noreferrer"><Image src='/project-pictures/bartcash.png' width='280' height='150' className='picture-style' title='Click to view' alt='Reality Pacesetters Foundation' /></a>
+            <h3 style={{ color: 'white' }}>BartCash</h3>
+        </Col>
+        
+        <Col className='space' sm={24} md={6}>
+        <a href='https://realitypacesettersfoundation.ng' target='_blank' rel="noreferrer"><Image src='/project-pictures/reality-pacesetters.png' width='280' height='150' className='picture-style' title='Click to view' alt='Reality Pacesetters Foundation' /></a>
+            <h3 style={{ color: 'white' }}>Reality Pace Setters Foundation (NGO)</h3>
+        </Col>
+
+        <Col className='space' sm={24} md={6}>
+        <a href='https://wearz.netlify.app/' target='_blank' rel="noreferrer"><Image src='/project-pictures/wearz.png' width='280' height='150' className='picture-style' title='Click to view' alt='Reality Pacesetters Foundation' /></a>
+            <h3 style={{ color: 'white' }}>Wearz</h3>
         </Col>
 
         </Row>
@@ -68,7 +86,13 @@ export default function RecipeReviewCard() {
         .picture-style {
           border-radius: 10px;
           border: 5px solid white;
+          transition: transform .5s;
           margin-bottom: 20px;
+        }
+
+        .picture-style:hover {
+          transform: scale(1.5);
+          border-radius: 10px;
         }
 
         `}
